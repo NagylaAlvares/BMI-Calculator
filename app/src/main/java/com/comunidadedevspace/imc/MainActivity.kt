1ba2be21794e6614d0154a0e5d01ca57c243349f
NagylaAlvares <nagypersonal@gmail.com>
2024-12-11 19:58:16 +0100

    Home Page editText

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,31 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // create a variable and associate with UI component <EditText>
+
+        val edtWeight = findViewById<TextInputEditText>(R.id.edt_weight)
+        val edtHeight = findViewById<TextInputEditText>(R.id.edt_height)
+
+        val btnCalculate = findViewById<Button>(R.id.btn_calculate)
+
+
+        btnCalculate.setOnClickListener{
+
+            val weight = edtWeight.text
+            val height = edtHeight.text
+            println("Button in action!!! " + weight)
+            println("Height button in action " + height)
+        }
+
+
+
     }
 }
